5ec197d3e8218f572af08c920a5bc16b6d1e6d53
Andrei Picus <office.nightcrawler@gmail.com>
2016-02-11 20:55:27 +0200

    Enable running the tests in the browser
    Makes debugging tests easier. Might come back later to this and remove some
    duplication. Might even change the Karma configs to use the new bundles instead.

---
+++ tests/unit/webpack.config.babel.js
@@ -0,0 +1,12 @@
+import baseConfig from '../../webpack.base.js';
+import path from 'path';
+
+
+export default Object.assign({}, baseConfig, {
+  entry: path.join(__dirname, 'index.js'),
+
+  output: {
+    filename: 'unit.js',
+    path: path.join(__dirname, '..', '..', 'build')
+  }
+})
